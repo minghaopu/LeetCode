4ad09b555a80505d93dead8862d4f86e09b3e533
roneilPMH <roneil_PMH@Hotmail.com>
2016-04-20 11:55:35 -0400

    Finish question 283;

---
+++ LeetCode/283_Move_Zeros.cpp
@@ -0,0 +1,38 @@
+//
+//  283_Move_Zeros.cpp
+//  LeetCode
+//
+//  Created by roneil on 16/4/20.
+//  Copyright © 2016年 Minghao Pu. All rights reserved.
+//
+
+#include "Header.h"
+using namespace std;
+
+class Solution {
+private:
+    vector<int> nums;
+public:
+    Solution(vector<int>& n) {
+        nums = n;
+    }
+    void moveZeros() {
+        int i = -1; // non-zero position
+        int j = 0;  // zero postion
+        int length = nums.size();
+        int temp;
+        while (j<length) {
+            if (nums[j] == 0) {
+                j++;
+                continue;
+            }
+            i++;
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            j++;
+        }
+    }
+    
+};
+
---
+++ LeetCode/Header.h
@@ -0,0 +1,19 @@
+//
+//  Header.h
+//  LeetCode
+//
+//  Created by roneil on 16/4/20.
+//  Copyright © 2016年 Minghao Pu. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <utility>
+#include <unordered_map>
+#include <unordered_set>
+
+#endif /* Header_h */
