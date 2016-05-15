baa7abba0b86e6d6b8aa93b5a329b1fbed20b515
roneil <roneil_PMH@hotmail.com>
2016-05-14 21:58:20 -0400

    Modify 118;

--- LeetCode/118_Pascal_Triangle.cpp
+++ LeetCode/118_Pascal_Triangle.cpp
@@ -9,21 +9,31 @@
 #include "Header.h"
 class Solution {
 public:
+//    vector<vector<int>> generate(int numRows) {
+//        vector<vector<int>> result(numRows);
+//        for (int i = 0; i < numRows; i++) {
+//            result[i].resize(i+1);
+//            for (int j = 0; j <= i; j++) {
+//                result[i][j] = combinations(i,j);
+//            }
+//        }
+//        return result;
+//    }
+//    int combinations(int n, int k) {
+//        int max = k > n - k ? k : n-k;
+//        int result = 1;
+//        for (int i = 1; i <= n - max; i++) {
+//            result = result * (max + i) / i;
+//        }
+//        return result;
+//    }
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> result(numRows);
         for (int i = 0; i < numRows; i++) {
-            result[i].resize(i+1);
-            for (int j = 0; j <= i; j++) {
-                result[i][j] = combinations(i,j);
+            result[i].resize(i+1,1);
+            for (int j = 1; j < i; j++) {
+                result[i][j] = result[i-1][j-1] + result[i-1][j];
             }
-        }
-        return result;
-    }
-    int combinations(int n, int k) {
-        int max = k > n - k ? k : n-k;
-        int result = 1;
-        for (int i = 1; i <= n - max; i++) {
-            result = result * (max + i) / i;
         }
         return result;
     }
