985cdb67f8382d76996b957412f756aca474be41
roneilPMH <roneil_PMH@hotmail.com>
2016-10-02 13:39:28 -0400

    Finish Question 12;

--- LeetCode/Medium.cpp
+++ LeetCode/Medium.cpp
@@ -406,4 +406,17 @@ public:
         }
         return 0;
     }
+    /*
+     *
+     *      12. Integer to Roman
+     *
+     */
+    string intToRoman(int num) {
+        string M[4] = {"", "M", "MM", "MMM"};
+        string I[10] = {"","I","II","III","IV","V", "VI", "VII", "VIII", "IX"};
+        string X[10] = {"","X","XX","XXX","XL","L", "LX", "LXX", "LXXX", "XC"};
+        string C[10] = {"","C","CC","CCC","DC","D", "DC", "DCC", "DCCC", "CM"};
+        return  M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[(num%10)];
+    }
+
 };
