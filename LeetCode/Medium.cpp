e8e340336d62531399e478a786be7b33f463790c
roneilPMH <roneil_PMH@hotmail.com>
2016-11-01 15:59:15 -0400

    Finish question 386;

--- LeetCode/Medium.cpp
+++ LeetCode/Medium.cpp
@@ -2496,4 +2496,23 @@ public:
         }
         return res;
     }
+    /*
+     *
+     *     386. Lexicographical Numbers
+     *
+     */
+    vector<int> lexicalOrder(int n) {
+        vector<int> res(n);
+        int cur = 1;
+        for (int i = 1; i <= n; i++) {
+            res[i - 1] = cur;
+            if (cur * 10 <= n) cur *=10;
+            else if (cur % 10 != 9 && cur < n) cur++;
+            else {
+                while ((cur / 10) % 10 == 9) cur /= 10;
+                cur = cur / 10 + 1;
+            }
+        }
+        return res;
+    }
 };
