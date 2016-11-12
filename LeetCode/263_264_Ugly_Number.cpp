9689eb3a2a65a2330cd9e449c0380c637abfdbd8
roneil <roneil_PMH@hotmail.com>
2016-11-12 18:21:00 -0500

    Finish question 264;

--- LeetCode/263_264_Ugly_Number.cpp
+++ LeetCode/263_264_Ugly_Number.cpp
@@ -21,4 +21,19 @@ public:
         }
         return true;
     }
+    int nthUglyNumber(int n) {
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
+        vector<int> dp (n + 1);
+        int ptr2 = 1, ptr3 = 1, ptr5 = 1;
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = min(dp[ptr2] * 2, min(dp[ptr3] * 3, dp[ptr5] * 5));
+            if (dp[i] == dp[ptr2] * 2) ptr2++;
+            if (dp[i] == dp[ptr3] * 3) ptr3++;
+            if (dp[i] == dp[ptr5] * 5) ptr5++;
+        }
+        return dp[n];
+    }
 };
